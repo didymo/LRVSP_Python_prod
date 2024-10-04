3090acee327fb1b56d71fab88daf3639e8f14139
Ashley Maher <ashley.maher@didymodesigns.com.au>
2024-10-05 08:50:34 +1000

    adding files used to make code more readable

---
+++ config.py
@@ -0,0 +1,8 @@
+DRUPAL_PATH = "/extra/PhpstormProjects/LRVSP_Drupal"
+LOG_PATH = "/extra/PycharmProjects/LRVSP_Python_prod/file.log"
+DB_CONFIG = {
+    'user': 'LVRSPPython',
+    'password': 'LVRSPPython',
+    'host': '127.0.0.1',
+    'database': 'LVRSPPython'
+}
